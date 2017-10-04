de33cc4086bcb930aee2927c7c89b69c640c0f32
mikedpicks <mddesignstl@gmail.com>
2017-10-03 20:13:47 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {routing} from './app.routing';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {HomeComponent} from './components/home/home.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
